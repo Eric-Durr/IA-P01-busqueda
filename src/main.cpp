0a054d303d6e29e4453783c35915aa1cfefffd32
Eric Dürr <eric.d.s.info@gmail.com>
2020-10-19 20:42:45 +0100

    2 + 4

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main (void) 
 {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello world im Elena" << std::endl;
+    std::cout << "This is 2 + 4: " << 2 + 4 << std::endl;
     return 0;
 }
