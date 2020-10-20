c2055db52fc121778edd6b9827affafa51e2f0b7
Elena Rijo <alu0101265421@ull.edu.es>
2020-10-20 16:33:31 +0100

    Desarrollo 1: cabeceras

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-
-int main (void) 
-{
-    std::cout << "Hello project" << std::endl;
-    return 0;
-}
---
+++ src/smart_lookup.cpp
@@ -0,0 +1,13 @@
+#include "../include/slot.hpp"
+#include "../include/environment.hpp"
+#include "../include/obstacle.hpp"
+#include "../include/smart_car.hpp"
+
+#include <iostream>
+
+
+int main (void) 
+{
+    std::cout << "Hello project" << std::endl;
+    return 0;
+}
